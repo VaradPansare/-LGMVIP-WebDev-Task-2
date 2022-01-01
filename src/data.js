e34e9e86ae9feea783337f9dff0f2d0990fa1900
VaradPansare <57860933+VaradPansare@users.noreply.github.com>
2022-01-01 18:46:17 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,51 @@
+const data = [
+    {
+      id: 1,
+      email: 's1mple@gmail.com',
+      user_info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+      first_name: 'NAVI',
+      last_name: 'S1mple',
+      avatar:'https://source.unsplash.com/D9HRdD46K0o/900x900',
+    },
+    {
+      id: 2,
+      email: 'kennys@gmail.com',
+      user_info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+      first_name: 'Kenny',
+      last_name: 'Schrub',
+      avatar: 'https://source.unsplash.com/C8Ta0gwPbQg/900x900',
+    },
+    {
+      id: 3,
+      email: 'skrossi@gmail.com',
+      user_info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+      first_name: 'GE',
+      last_name: 'SkRossi',
+      avatar: 'https://source.unsplash.com/iFgRcqHznqg/900x900',
+    },
+    {
+      id: 4,
+      email: 'tenz@gmail.com',
+      user_info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+      first_name: 'SEN',
+      last_name: 'Tenz',
+      avatar: 'https://source.unsplash.com/s2OisyY9YGU/900x900',
+    },
+    {
+      id: 5,
+      email: 'shahzam@gmail.com',
+      user_info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+      first_name: 'SEN',
+      last_name: 'Shahzam',
+      avatar: 'https://source.unsplash.com/_H6wpor9mjs/900x900',
+    },
+    {
+      id: 6,
+      email: 'scream@gmail.com',
+      user_info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+      first_name: 'TL',
+      last_name: 'Scream',
+      avatar: 'https://source.unsplash.com/e4aUQWMXaJs/900x900',
+    },
+  ];
+  export default data;
